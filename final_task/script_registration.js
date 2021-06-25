12381ecdd93328dbb6be49d8df357d7eab1fc9f1
Kseniya Ivanova <kseniya.i369@mail.ru>
2021-06-25 15:34:21 +0300

    final

---
+++ final_task/script_registration.js
@@ -0,0 +1,30 @@
+document.body.style.backgroundImage = 'url(https://catherineasquithgallery.com/uploads/posts/2021-02/1614384091_11-p-svetlii-fon-dlya-avatarki-11.jpg)';
+
+
+let form = document.createElement("form");
+
+let fieldset1 = document.createElement("fieldset");
+let legend = document.createElement("legend");
+legend.innerHTML = "Регистрация";
+form.style.background = "white";
+form.style.width = "250px";
+form.style.height = "250px";
+
+let inputEmail = document.createElement("input");
+inputEmail.type = "email";
+inputEmail.required = true;
+inputEmail.placeholder = "Логин";
+inputEmail.autocomplete = "on";
+
+let inputPassword = document.createElement("input");
+inputPassword.type.password = "pass";
+inputPassword.required = true;
+inputPassword.placeholder = "Пароль";
+
+
+let btnSgnIn = document.createElement("button");
+btnSgnIn.innerHTML = "Регистрация";
+form.append(fieldset1);
+fieldset1.prepend(legend, inputEmail, inputPassword, btnSgnIn);
+document.body.append(form);
+
